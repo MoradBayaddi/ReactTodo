a0563d8abb4d932b4b5ef5f101d4404c9a96bb22
Morad Bayaddi <mourad201630@gmail.com>
2024-02-12 03:15:34 +0100

    checking update

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ const App = () => {
     <div className='bg-[#000814] text-[#ffc300] py-10 px-5 lg:px-0  h-[100vh]'>
 
       <h1 className="text-3xl overflow-hidden	 mx-20 text-center  xl:text-left xl:font-bold ">
-        YAMI
+        3zawi
       </h1>
       
       <div className=' flex justify-center h-auto  w-full  '>
